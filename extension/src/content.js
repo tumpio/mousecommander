57c812940f2edf1ef5cb35616fbfbb077d3b28ec
tumpio <tumpio@post.com>
2019-09-15 10:53:08 +0300

    Fix prevent of new tab open

--- extension/src/content.js
+++ extension/src/content.js
@@ -2,31 +2,39 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
+/**
+ * Prevent context menu on secondary mouse down commands.
+ */
 browser.runtime.onMessage.addListener(request => {
     if (request.preventContextMenu) {
-        window.addEventListener("contextmenu", preventOnce, false);
+        window.addEventListener("contextmenu", prevent, { once: true }, false);
     } else if (request.reset) {
-        window.removeEventListener("contextmenu", preventOnce, false);
+        window.removeEventListener("contextmenu", prevent, false);
     }
 });
 
-function preventOnce(event) {
-    event.preventDefault();
-    window.removeEventListener("contextmenu", preventOnce, false);
-}
-
+/**
+ * Prevent page zoom on mouse down + wheel commands.
+ */
 window.addEventListener("wheel", function (e) {
     if (e.buttons !== 0) {
-        e.preventDefault();
+        prevent(e);
     }
 }, false);
 
-window.addEventListener("click", function (e) {
+/**
+ * Prevent link open on middle click commands.
+ */
+window.addEventListener("auxclick", function (e) {
     if (e.button === 1 && e.buttons !== 0) {
-        e.preventDefault();
+        prevent(e);
     }
 }, false);
 
 browser.runtime.connect({
     "name": "content"
 });
+
+function prevent(event) {
+    event.preventDefault();
+}
